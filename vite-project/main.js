bd8f7ef53d206c04a7e4e1a5e3fe1ae83c34d44b
Ramesh <22.rameshlg@gmail.com>
2022-12-20 13:44:19 +0530

    Add metallic and roughness to the sphere

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,6 +1,7 @@
 import  * as THREE from 'three';
 import { Camera } from 'three';
 import "./style.css"
+import gsap from 'gsap';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 //Scene
@@ -9,7 +10,8 @@ const scene = new THREE.Scene();
 //Create sphere
 const geometry = new THREE.SphereGeometry(3,64,64);
 const material = new THREE.MeshStandardMaterial({
-    color: "00ff83",
+    color: "#08f26e",
+    roughness : 0.01,
 })
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh)
@@ -23,12 +25,13 @@ const sizes = {
 //Light
 const light = new THREE.PointLight(0xffffff, 1, 100)
 light.position.set(0,10,10) 
+light.intensity = 1.25
 scene.add(light)
 
 //Camera
 const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 100)
 camera.position.z = 20;
-scene.add(Camera)
+scene.add(camera)
 
 
 //Renderer
@@ -44,7 +47,8 @@ controls.enableDamping = true;
 controls.enablePan = false;
 controls.enableZoom = false;
 controls.autoRotate = true;
-controls.autoRotateSpeed = 5;
+controls.autoRotateSpeed = 5
+
 
 
 //Resizes
@@ -59,8 +63,40 @@ window.addEventListener("resize", () => {
 })
 
 const loop = () =>{
-    
+    controls.update();
     renderer.render(scene, camera)
     window.requestAnimationFrame(loop)
 }
 loop()
+
+//Timeline (Animation)
+const t1 = gsap.timeline({ defaults: {duration: 1}})
+t1.fromTo(mesh.scale, {x:0, y:0, z:0}, {x:1, y:1, z:1})
+t1.fromTo('nav', {y:'-100%'}, {y:'0%'})
+t1.fromTo("title", { opacity:0}, {opacity : 1})
+
+//Mouse animation color change
+let mouseDown = false;
+let rgb = [];
+
+window.addEventListener("mousedown", () => (mouseDown = true))
+window.addEventListener("mouseUp", () => (mouseDown = false))
+
+window.addEventListener('mousemove', (e) =>{
+    if(mouseDown){
+        rgb = [
+            Math.round((e.pageX / sizes.width) * 255),
+            Math.round((e.pageY / sizes.height) * 255),
+            150,
+        ]
+        //color change
+        let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
+        gsap.to(mesh.material.color, {
+           r: newColor.r,
+           g: newColor.g,
+           b: newColor.b, 
+        })
+
+    }
+})
+
